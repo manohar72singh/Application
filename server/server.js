2c2eafb809f8a27f916c9ff5fe23ff691af35a8b
Manoharsingh <manohar72singh@gmail.com>
2025-07-13 16:09:03 +0530

    change "cors"

--- server/server.js
+++ server/server.js
@@ -5,16 +5,35 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./config/mongodb");
 const authRouter = require("./Routes/authRoutes");
 const userRouter = require("./Routes/userRoutes");
-const allowedOrigins = ["http://localhost:5173"];
+// const allowedOrigins = ["http://localhost:5173"];
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
 // app.use(cors({ origin: allowedOrigins, credentials: true }));
 // const port = process.env.PORT || 1001;
+// app.use(
+//   cors({
+//     origin: "https://auth-application-7th3.vercel.app", // ✅ allow your Vercel domain
+//     credentials: true,
+//   })
+// );
+
+// 👇 ALLOW your Vercel frontend URL
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://auth-application-7th3.vercel.app",
+];
+
 app.use(
   cors({
-    origin: "https://auth-application-7th3.vercel.app", // ✅ allow your Vercel domain
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
